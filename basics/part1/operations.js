d4c96500160d6c15dc90a6e9e8b921149ed81665
devHksingh <hksingh.dev@gmail.com>
2024-09-26 08:47:59 +0530

    oprator

---
+++ basics/part1/operations.js
@@ -0,0 +1,57 @@
+let score = 102
+let bonus = 25
+
+let totalScore = score + bonus
+
+let addition = 4+5 
+
+let substract = 9-3;
+let mult = 2*5;
+let div = 10/2;
+let reminder = 9%2
+let pow = 2**3 
+
+// Increment & Decrement
+
+let newScore = 110;
+newScore ++;
+// ++newScore;
+let credits = 88;
+credits--
+
+// assigment oprator
+
+let num =2
+
+num +=1;
+console.log(num);
+
+
+//  Cmoparion opertaion
+
+let num1 = 4
+let num2 = 4
+let num3 = 6
+
+console.log(num1 == num2); // true
+console.log(num1 != num3); // true
+console.log(num1 > num3); 
+console.log(num1 < num3); 
+
+// logical op
+
+/*
+  && > and
+  || > or
+  ! -> not /reverse
+*/
+
+let isloggedIn = true
+let ispaid = false
+
+console.log(isloggedIn && ispaid);
+
+let isEmailUser = true;
+let isGoogleUser = false;
+
+console.log(isEmailUser || isGoogleUser);
