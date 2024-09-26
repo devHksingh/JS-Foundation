0d511d830ed83a6d853623589cff478331fa19fe
devHksingh <hksingh.dev@gmail.com>
2024-09-26 08:10:58 +0530

    dataype variable printing

---
+++ basics/part1/printing.js
@@ -0,0 +1,43 @@
+console.log("hello");
+process.stdout.write('JS')
+process.stdout.write('hello')
+
+console.table({city:new delhi})
+
+
+/*
+Data Type 
+String
+Number
+Boolean
+Bigint
+
+Undefined
+null
+
+Object
+
+Symbol
+
+*/
+
+// var score = 102;
+let score = 102;
+let name = "Harshit"
+let isLoggedIn = false
+
+// object
+let teaType =["lemon tea", "orange tea"]
+let user ={firstname:"Harshit", middleName:"Kumar", lastName:"Singh"}
+
+let getScore = score;
+
+console.log(getScore);
+
+let gameName = "spiderMan"
+gameName ="batman"
+
+const userName = "Jhone"
+console.log(userName);
+userName = "Doe"
+console.log(userName); //type error
